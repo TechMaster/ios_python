af45c80b5cf4c80532fce6c95559b12d211e599a
techmaster <cuong@techmaster.vn>
2019-09-26 15:26:50 +0700

    Validate Field

--- ios/UserManageClient/UserManageClient/Screens/EditScreen.swift
+++ ios/UserManageClient/UserManageClient/Screens/EditScreen.swift
@@ -9,6 +9,8 @@
 import UIKit
 import Eureka
 import ImageRow
+import Alamofire
+
 class EditScreen: FormViewController  {
     var user: User!
     var editMode: Bool!  //True if edit existing record, False if add new record
@@ -22,7 +24,7 @@ class EditScreen: FormViewController  {
     
     init() {
         super.init(style: UITableView.Style.grouped)
-        editMode = false
+        editMode = false  // Add Mode
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -33,35 +35,42 @@ class EditScreen: FormViewController  {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(onSave))
         
         form +++ TextRow(){ row in
-                row.tag = "Name"
-                row.title = "Name"
-                row.placeholder = "Enter text here"
+            row.tag = "name"
+            row.title = "Name"
+            row.placeholder = "Enter text here"
+            row.add(rule: RuleRequired())
+            row.validationOptions = .validatesOnDemand
             }
             <<< EmailRow(){ row in
-                row.tag = "Email"
+                row.tag = "email"
                 row.title = "Email"
                 row.placeholder = "And email here"
+                row.add(rule: RuleRequired())
+                row.add(rule: RuleEmail())
+                row.validationOptions = .validatesOnDemand
             }
             <<< PasswordRow() {row in
-                row.tag = "Pass"
-                row.title = "Pass"
+                row.tag = "password"
+                row.title = "Password"
                 row.placeholder = "Password here"
+                row.add(rule: RuleRequired())
+                row.validationOptions = .validatesOnDemand
             }
             <<< ImageRow() {
-                $0.tag = "Photo"
+                $0.tag = "photo"
                 $0.title = "Photo"
                 $0.sourceTypes = [.PhotoLibrary, .SavedPhotosAlbum]
                 $0.clearAction = .yes(style: .default)
-            }
+        }
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         if user != nil {
             navigationItem.title = "Edit User"
-            form.rowBy(tag: "Name")?.value = user.name
-            form.rowBy(tag: "Email")?.value = user.email
-            form.rowBy(tag: "Pass")?.value = user.pass
+            form.rowBy(tag: "name")?.value = user.name
+            form.rowBy(tag: "email")?.value = user.email
+            form.rowBy(tag: "password")?.value = user.pass
         } else {
             navigationItem.title = "Add User"
         }
@@ -71,12 +80,40 @@ class EditScreen: FormViewController  {
     
     // Đoạn này sẽ lưu dữ liệu lên server
     @objc func onSave() {
+        form.validate()
+        for row in form.rows {
+            print(row.validationErrors)
+        }
         let valuesDictionary = form.values()
-        print(valuesDictionary)
-       
+        
+        if editMode {
+            
+        } else {
+            let queue = DispatchQueue(label: "vn.techmaster.api", qos: .background, attributes: .concurrent)
+            let base_url = Server.shared.baseURL()
+            let parameters: Parameters = [
+                "name": valuesDictionary["name"]! ?? "",
+                "email": valuesDictionary["email"]! ?? "",
+                "password": valuesDictionary["password"]! ?? ""
+            ]
+            Alamofire.request(base_url + "user",
+                              method:.post,
+                              parameters: parameters,
+                              encoding: URLEncoding.httpBody).responseJSON(queue: queue) { response in
+                                switch response.result {
+                                case .success(let value):
+                                    let new_id = value as! Int
+                                    print(new_id)
+                                    
+                                case .failure(let error):
+                                    print(error)
+                                }
+            }
+        }
+        
     }
 }
-    
+
 
 
 
